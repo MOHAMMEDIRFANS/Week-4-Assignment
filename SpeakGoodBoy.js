1c97195109eff17d1251daa0eca5d1a551e0bf9c
MOHAMMEDIRFANS <35970861+MOHAMMEDIRFANS@users.noreply.github.com>
2018-02-21 19:27:25 +0530

    Add files via upload

---
+++ SpeakGoodBoy.js
@@ -0,0 +1,28 @@
+var JohnGreeter = {};
+var JenGreeter = {};
+var JasonGreeter = {};
+var JimGreeter = {};
+
+JohnGreeter.name = "John";
+JenGreeter.name = "Jen";
+JasonGreeter.name = "Jason";
+JimGreeter.name = "Jim";
+
+JohnGreeter.sayGoodBoy = function () {
+console.log("GoodBoy " + JohnGreeter.name);
+}
+
+
+JenGreeter.sayGoodBoy = function () {
+console.log("GoodBoy " + JenGreeter.name);
+}
+
+
+JasonGreeter.sayGoodBoy = function () {
+console.log("GoodBoy " + JasonGreeter.name);
+}
+
+
+JimGreeter.sayGoodBoy = function () {
+console.log("GoodBoy " + JimGreeter.name);
+}
